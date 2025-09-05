82b152430a8e6d9d058f08eb801b0e7e0f836c53
andersonkaiti <anderkaiti@gmail.com>
2025-09-04 22:20:25 -0300

    :wrench: chore(database): Reestruturação do schema do Prisma com o enum UserRole, novos modelos User, News, Teams e TeamMembers, e remoção de tabelas/campos obsoletos

--- prisma/migrations/20250531024720_remove_unique_teammember_constraint/migration.sql
+++
@@ -1,5 +0,0 @@
--- DropIndex
-DROP INDEX "team_members_team_id_key";
-
--- DropIndex
-DROP INDEX "team_members_user_id_key";
--- prisma/migrations/20250608201450_add_optional_description_field_to_teammember_table/migration.sql
+++
@@ -1,2 +0,0 @@
--- AlterTable
-ALTER TABLE "team_members" ADD COLUMN     "description" TEXT;
